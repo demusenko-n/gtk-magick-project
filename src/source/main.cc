d5f95e15fe18eb14b43f071f0654099b62def647
mdemusenko <mdemusenko@outlook.com>
2023-03-28 19:03:27 +0300

    Create CMake C++ project

---
+++ src/source/main.cc
@@ -0,0 +1,11 @@
+#include <gtkmm/application.h>
+#include <Magick++.h>
+#include "helloworld.h"
+
+int main(int argc, char* argv[])
+{
+	auto app = Gtk::Application::create("org.gtkmm.example");
+
+	//Shows the window and returns when it is closed.
+	return app->make_window_and_run<HelloWorld>(argc, argv);
+}
